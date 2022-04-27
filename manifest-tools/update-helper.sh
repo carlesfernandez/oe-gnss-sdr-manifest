081c27e068723680f5b8c92f019826402d4160f2
Carles Fernandez <carles.fernandez@gmail.com>
2022-04-27 09:56:18 +0200

    update-helper.sh: add kirkstone

--- manifest-tools/update-helper.sh
+++ manifest-tools/update-helper.sh
@@ -21,21 +21,19 @@ BASEDIR=$PWD
 
 REPO_OE_CORE="https://github.com/openembedded/openembedded-core"
 REPO_META_OE="https://github.com/openembedded/meta-openembedded"
-REPO_META_QT4="git://git.yoctoproject.org/meta-qt4"
-REPO_META_QT4_BROWSER="https://git.yoctoproject.org/cgit/cgit.cgi/meta-qt4"
+REPO_META_QT4="https://git.yoctoproject.org/meta-qt4"
 REPO_META_QT5="https://github.com/meta-qt5/meta-qt5"
 REPO_META_QT5_EXTRA="https://github.com/schnitzeltony/meta-qt5-extra"
 REPO_META_SDR="https://github.com/balister/meta-sdr"
-REPO_META_VIRTUALIZATION="git://git.yoctoproject.org/meta-virtualization"
-REPO_META_VIRTUALIZATION_BROWSER="https://git.yoctoproject.org/meta-virtualization"
+REPO_META_VIRTUALIZATION="https://git.yoctoproject.org/meta-virtualization"
 REPO_META_XILINX="https://github.com/Xilinx/meta-xilinx"
 REPO_META_XILINX_TOOLS="https://github.com/Xilinx/meta-xilinx-tools"
 REPO_META_ADI="https://github.com/analogdevicesinc/meta-adi"
 REPO_HDL="https://github.com/analogdevicesinc/hdl"
-REPO_META_RASPBERRYPI="git://git.yoctoproject.org/meta-raspberrypi"
-REPO_META_RASPBERRYPI_BROWSER="https://git.yoctoproject.org/cgit/cgit.cgi/meta-raspberrypi"
-REPO_META_INTEL="git://git.yoctoproject.org/meta-intel"
-REPO_META_INTEL_BROWSER="https://git.yoctoproject.org/cgit/cgit.cgi/meta-intel"
+REPO_META_RASPBERRYPI="https://git.yoctoproject.org/meta-raspberrypi"
+REPO_META_INTEL="https://git.yoctoproject.org/meta-intel"
+REPO_META_TI="https://git.yoctoproject.org/meta-ti"
+REPO_META_ARM="https://git.yoctoproject.org/meta-arm"
 REPO_META_SW_UPDATE="https://github.com/sbabic/meta-swupdate"
 
 echo -e "Getting the latest commits in the ${BRANCH} branch..."
@@ -132,6 +130,10 @@ if [ ${BRANCH} == "gatesgarth" ]
     then
         SPECIAL_SDR_BRANCH="dunfell"
 fi
+if [ ${BRANCH} == "kirkstone" ]
+    then
+        SPECIAL_SDR_BRANCH="master"
+fi
 if [ -d "meta-sdr" ]
     then
         cd meta-sdr
@@ -203,31 +205,34 @@ if [ ${BRANCH} == "gatesgarth" ]
     then
         SPECIAL_XILINX_BRANCH="rel-v2021.2"
 fi
-if [ -d "meta-xilinx" ]
+if [ ${BRANCH} != "kirkstone" ]
     then
-        cd meta-xilinx
-        exists_in_remote=$(git ls-remote --heads origin ${SPECIAL_XILINX_BRANCH})
-        if [[ -z ${exists_in_remote} ]]
-           then
-               COMMIT_META_XILINX="Unknown"
-           else
-               git checkout ${SPECIAL_XILINX_BRANCH}
-               git pull origin ${SPECIAL_XILINX_BRANCH}
-               COMMIT_META_XILINX=$(git rev-parse HEAD)
-        fi
-    else
-        git clone ${REPO_META_XILINX}
-        cd meta-xilinx
-        exists_in_remote=$(git ls-remote --heads origin ${SPECIAL_XILINX_BRANCH})
-        if [[ -z ${exists_in_remote} ]]
+        if [ -d "meta-xilinx" ]
             then
-                COMMIT_META_XILINX="Unknown"
+                cd meta-xilinx
+                exists_in_remote=$(git ls-remote --heads origin ${SPECIAL_XILINX_BRANCH})
+                if [[ -z ${exists_in_remote} ]]
+                   then
+                       COMMIT_META_XILINX="Unknown"
+                   else
+                       git checkout ${SPECIAL_XILINX_BRANCH}
+                       git pull origin ${SPECIAL_XILINX_BRANCH}
+                       COMMIT_META_XILINX=$(git rev-parse HEAD)
+                fi
             else
-                git checkout ${SPECIAL_XILINX_BRANCH}
-                COMMIT_META_XILINX=$(git rev-parse HEAD)
+                git clone ${REPO_META_XILINX}
+                cd meta-xilinx
+                exists_in_remote=$(git ls-remote --heads origin ${SPECIAL_XILINX_BRANCH})
+                if [[ -z ${exists_in_remote} ]]
+                    then
+                        COMMIT_META_XILINX="Unknown"
+                    else
+                        git checkout ${SPECIAL_XILINX_BRANCH}
+                        COMMIT_META_XILINX=$(git rev-parse HEAD)
+                fi
         fi
+        cd ..
 fi
-cd ..
 
 # meta-xilinx-tools
 SPECIAL_XILINX_TOOLS_BRANCH=${BRANCH}
@@ -240,7 +245,7 @@ if [ ${BRANCH} == "zeus" ]
        SPECIAL_XILINX_TOOLS_BRANCH="rel-v2020.3"
 fi
 
-if [ ${BRANCH} == "thud" ] || [ ${BRANCH} == "zeus" ]
+if [ ${BRANCH} == "thud" ] || [ ${BRANCH} == "zeus" ] || [ ${BRANCH} == "honister" ]
    then
        if [ -d "meta-xilinx-tools" ]
            then
@@ -400,6 +405,66 @@ if [ -d "meta-intel" ]
 fi
 cd ..
 
+# meta-ti
+if [ ${BRANCH} == "dunfell" ]
+    then
+        if [ -d "meta-ti" ]
+            then
+                cd meta-ti
+                exists_in_remote=$(git ls-remote --heads origin ${BRANCH})
+                if [[ -z ${exists_in_remote} ]]
+                   then
+                       COMMIT_META_TI="Unknown"
+                   else
+                       git checkout ${BRANCH}
+                       git pull origin ${BRANCH}
+                       COMMIT_META_TI=$(git rev-parse HEAD)
+                fi
+        else
+            git clone ${REPO_META_TI}
+            cd meta-ti
+            exists_in_remote=$(git ls-remote --heads origin ${BRANCH})
+            if [[ -z ${exists_in_remote} ]]
+                then
+                    COMMIT_META_TI="Unknown"
+                else
+                    git checkout ${BRANCH}
+                    COMMIT_META_TI=$(git rev-parse HEAD)
+            fi
+        fi
+    cd ..
+fi
+
+# meta-arm
+if [ ${BRANCH} == "dunfell" ]
+    then
+        if [ -d "meta-ti" ]
+            then
+                cd meta-arm
+                exists_in_remote=$(git ls-remote --heads origin ${BRANCH})
+                if [[ -z ${exists_in_remote} ]]
+                   then
+                       COMMIT_META_ARM="Unknown"
+                   else
+                       git checkout ${BRANCH}
+                       git pull origin ${BRANCH}
+                       COMMIT_META_ARM=$(git rev-parse HEAD)
+                fi
+        else
+            git clone ${REPO_META_ARM}
+            cd meta-arm
+            exists_in_remote=$(git ls-remote --heads origin ${BRANCH})
+            if [[ -z ${exists_in_remote} ]]
+                then
+                    COMMIT_META_ARM="Unknown"
+                else
+                    git checkout ${BRANCH}
+                    COMMIT_META_ARM=$(git rev-parse HEAD)
+            fi
+        fi
+    cd ..
+fi
+
 # meta-swupdate
 if [ -d "meta-swupdate" ]
     then
@@ -464,7 +529,7 @@ if [ ${BRANCH} == "rocko" ] || [ ${BRANCH} == "sumo" ] || [ ${BRANCH} == "thud" 
            then
                echo -e "meta-qt4 last commit:            ${COMMIT_META_QT4}"
            else
-               echo -e "meta-qt4 last commit:            ${COLOR_WARNING}${COMMIT_META_QT4}${COLOR_RESET} Check ${REPO_META_QT4_BROWSER}/log/?h=${BRANCH}"
+               echo -e "meta-qt4 last commit:            ${COLOR_WARNING}${COMMIT_META_QT4}${COLOR_RESET} Check ${REPO_META_QT4}/log/?h=${BRANCH}"
        fi
 fi
 
@@ -507,25 +572,28 @@ if [ ${BRANCH} == "honister" ]
            then
                echo -e "meta-virtualization last commit: ${COMMIT_META_VIRTUALIZATION}"
            else
-               echo -e "meta-virtualization last commit: ${COLOR_WARNING}${COMMIT_META_VIRTUALIZATION}${COLOR_RESET} Check ${REPO_META_VIRTUALIZATION_BROWSER}/log/?h=${BRANCH}"
+               echo -e "meta-virtualization last commit: ${COLOR_WARNING}${COMMIT_META_VIRTUALIZATION}${COLOR_RESET} Check ${REPO_META_VIRTUALIZATION}/log/?h=${BRANCH}"
        fi
 fi
 
 # Display latest commit for meta-xilinx
-if grep -q ${COMMIT_META_XILINX} "${BASEDIR}/../default.xml"
+if [ ${BRANCH} != "kirkstone" ]
     then
-        echo -e "meta-xilinx last commit:         ${COMMIT_META_XILINX}"
-    else
-        if [ ${COMMIT_META_XILINX} == "Unknown" ]
+        if grep -q ${COMMIT_META_XILINX} "${BASEDIR}/../default.xml"
             then
-                echo -e "meta-xilinx last commit:         ${COLOR_INFO}${COMMIT_META_XILINX}${COLOR_RESET}"
+                echo -e "meta-xilinx last commit:         ${COMMIT_META_XILINX}"
             else
-                echo -e "meta-xilinx last commit:         ${COLOR_WARNING}${COMMIT_META_XILINX}${COLOR_RESET} Check ${REPO_META_XILINX}/tree/${SPECIAL_XILINX_BRANCH}"
+                if [ ${COMMIT_META_XILINX} == "Unknown" ]
+                    then
+                        echo -e "meta-xilinx last commit:         ${COLOR_INFO}${COMMIT_META_XILINX}${COLOR_RESET}"
+                    else
+                        echo -e "meta-xilinx last commit:         ${COLOR_WARNING}${COMMIT_META_XILINX}${COLOR_RESET} Check ${REPO_META_XILINX}/tree/${SPECIAL_XILINX_BRANCH}"
+                fi
         fi
 fi
 
 # Display latest commit for meta-xilinx-tools
-if [ ${BRANCH} == "thud" ] || [ ${BRANCH} == "zeus" ]
+if [ ${BRANCH} == "thud" ] || [ ${BRANCH} == "zeus" ] || [ ${BRANCH} == "honister" ]
     then
         if grep -q ${COMMIT_META_XILINX_TOOLS} "${BASEDIR}/../default.xml"
             then
@@ -562,7 +630,7 @@ if grep -q ${COMMIT_META_RASPI} "${BASEDIR}/../default.xml"
     then
         echo -e "meta-raspberrypi last commit:    ${COMMIT_META_RASPI}"
     else
-        echo -e "meta-raspberrypi last commit:    ${COLOR_WARNING}${COMMIT_META_RASPI}${COLOR_RESET} Check ${REPO_META_RASPBERRYPI_BROWSER}/log/?h=${BRANCH}"
+        echo -e "meta-raspberrypi last commit:    ${COLOR_WARNING}${COMMIT_META_RASPI}${COLOR_RESET} Check ${REPO_META_RASPBERRYPI}/log/?h=${BRANCH}"
 fi
 
 # Display latest commit for meta-intel
@@ -570,15 +638,40 @@ if grep -q ${COMMIT_META_INTEL} "${BASEDIR}/../default.xml"
     then
         echo -e "meta-intel last commit:          ${COMMIT_META_INTEL}"
     else
-        echo -e "meta-intel last commit:          ${COLOR_WARNING}${COMMIT_META_INTEL}${COLOR_RESET} Check ${REPO_META_INTEL_BROWSER}/log/?h=${BRANCH}"
+        echo -e "meta-intel last commit:          ${COLOR_WARNING}${COMMIT_META_INTEL}${COLOR_RESET} Check ${REPO_META_INTEL}/log/?h=${BRANCH}"
+fi
+
+# Display latest commit for meta-ti
+if [ ${BRANCH} == "dunfell" ]
+    then
+        if grep -q ${COMMIT_META_TI} "${BASEDIR}/../default.xml"
+            then
+                echo -e "meta-ti last commit:             ${COMMIT_META_TI}"
+            else
+                echo -e "meta-ti last commit:             ${COLOR_WARNING}${COMMIT_META_TI}${COLOR_RESET} Check ${REPO_META_TI}/log/?h=${BRANCH}"
+        fi
+fi
+
+# Display latest commit for meta-arm
+if [ ${BRANCH} == "dunfell" ]
+    then
+        if grep -q ${COMMIT_META_ARM} "${BASEDIR}/../default.xml"
+            then
+                echo -e "meta-arm last commit:            ${COMMIT_META_ARM}"
+            else
+                echo -e "meta-arm last commit:            ${COLOR_WARNING}${COMMIT_META_ARM}${COLOR_RESET} Check ${REPO_META_ARM}/log/?h=${BRANCH}"
+        fi
 fi
 
 # Display latest commit for meta-swupdate
-if grep -q ${COMMIT_META_SWUPDATE} "${BASEDIR}/../default.xml"
+if [ ${BRANCH} != "kirkstone" ]
     then
-        echo -e "meta-swupdate last commit:       ${COMMIT_META_SWUPDATE}"
-    else
-        echo -e "meta-swupdate last commit:       ${COLOR_INFO}${COMMIT_META_SWUPDATE}${COLOR_RESET} Check ${REPO_META_SW_UPDATE}/tree/${BRANCH}"
+        if grep -q ${COMMIT_META_SWUPDATE} "${BASEDIR}/../default.xml"
+            then
+                echo -e "meta-swupdate last commit:       ${COMMIT_META_SWUPDATE}"
+            else
+                echo -e "meta-swupdate last commit:       ${COLOR_INFO}${COMMIT_META_SWUPDATE}${COLOR_RESET} Check ${REPO_META_SW_UPDATE}/tree/${BRANCH}"
+        fi
 fi
 
 cd $BASEDIR
