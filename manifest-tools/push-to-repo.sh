9f398f6a763b9bf0eaa062bd40a83bc3b01a636c
Carles Fernandez <carles.fernandez@gmail.com>
2022-12-09 15:01:11 +0100

    Add push-to-repo.sh script. Improve update-from-upstream.sh script: add version, help, sanity checks. Add sources/* to .gitignore so update-helper-sh can be called from the root folder

--- manifest-tools/push-to-repo.sh
+++ manifest-tools/push-to-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# Update all branches from the upstream repository
+# Push all branches to a repo
 #
-# Usage: ./update-from-upstream.sh
+# Usage: ./push-to-repo.sh [remote]
 #
 # SPDX-FileCopyrightText: 2022 Carles Fernandez-Prades <cfernandez(at)cttc.es>
 # SPDX-License-Identifier: MIT
@@ -15,10 +15,13 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 display_usage() {
-    echo "update-from-upstream.sh v$version - This script pulls all branches from the upstream repo."
-    echo " Supported branches: $branches"   
+    echo "push-to-repo.sh v$version - This script pushes all branches to a git repo."
+    echo " Supported branches: $branches"
     echo " Usage:"
-    echo "  ./update-from-upstream.sh"
+    echo "  ./push-to-repo.sh [remote]"
+    echo "  where:"
+    echo "   remote   Remote name (or full URL)"
+
 }
 
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
@@ -31,7 +34,7 @@ if [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
     exit 0
 fi
 
-upstream="https://github.com/carlesfernandez/oe-gnss-sdr-manifest"
+remote=${1:-"https://github.com/carlesfernandez/oe-gnss-sdr-manifest"}
 currentbranch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
 if [ -z "$currentbranch" ]; then
@@ -39,11 +42,11 @@ if [ -z "$currentbranch" ]; then
     exit 1
 fi
 
-git fetch $upstream 2> /dev/null || echo "Remote $upstream does not appear to be a valid git remote. Exiting." && exit 1
+git fetch "$remote" 2> /dev/null || echo "Remote $remote does not appear to be a git repository. Exiting." && exit 1
 
 for branch in $branches; do
     git checkout "$branch" || echo "Branch $branch is not known to git. Exiting." && exit 1
-    git pull $upstream "$branch" || echo "Something happened when pulling from the $branch branch. Aborting." && exit 1
+    git push "$remote" "$branch" || echo "Something happened when pushing to $remote/$branch. Aborting." && exit 1
 done
 
 git checkout "$currentbranch"
