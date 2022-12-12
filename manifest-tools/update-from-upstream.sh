716fc25b4f4eaee384e8e704df793f04bece15af
Carles Fernandez <carles.fernandez@gmail.com>
2022-12-12 10:51:26 +0100

    Improve scripts

--- manifest-tools/update-from-upstream.sh
+++ manifest-tools/update-from-upstream.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Update all branches from the upstream repository
 #
-# Usage: ./update-from-upstream.sh
+# Usage: ./update-from-upstream.sh [upstream]
 #
 # SPDX-FileCopyrightText: 2022 Carles Fernandez-Prades <cfernandez(at)cttc.es>
 # SPDX-License-Identifier: MIT
@@ -18,7 +18,9 @@ display_usage() {
     echo "update-from-upstream.sh v$version - This script pulls all branches from the upstream repo."
     echo " Supported branches: $branches"   
     echo " Usage:"
-    echo "  ./update-from-upstream.sh"
+    echo "  ./update-from-upstream.sh remote"
+    echo "  where:"
+    echo "   remote   [optional] upstream remote name (or full URL)"
 }
 
 if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
@@ -31,7 +33,13 @@ if [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
     exit 0
 fi
 
-upstream="https://github.com/carlesfernandez/oe-gnss-sdr-manifest"
+if [ "$#" -gt 1 ]; then
+   echo "Error: wrong number of parameters."
+   display_usage
+   exit 1
+fi
+
+upstream=${1:-"https://github.com/carlesfernandez/oe-gnss-sdr-manifest"}
 currentbranch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
 
 if [ -z "$currentbranch" ]; then
@@ -41,11 +49,9 @@ fi
 
 echo "Running update-from-upstream.sh v$version ..."
 
-git fetch $upstream
-
 for branch in $branches; do
     git checkout "$branch"
-    git pull $upstream "$branch"
+    git pull "$upstream" "$branch"
 done
 
 git checkout "$currentbranch"
